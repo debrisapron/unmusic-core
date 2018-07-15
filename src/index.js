abf99917f9d25a0a6cdb48fe58fcd093c5b33d0f
DEBRIS APRON <debrisapron@gmail.com>
2018-07-15 14:13:27 -0700

    Tidy

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ let mixScores = require('./mixScores')
 let umlangEval = require('./umlang/eval')
 
 // TODO Remove this. Too "clever".
-function wrapScoringFunction(fn) {
+let wrapFn = (fn) => {
   return fn.length === 1
     ? (thing) => fn(getScore(thing))
     : _.curry((options, thing) => fn(options, getScore(thing)))
 }
 
-let arpeg = wrapScoringFunction((opts, score) => {
+///////////////////////////////////////////////////////////////////////////////
+
+let arpeg = wrapFn((opts, score) => {
   score = _.clone(score)
   let [noteActions, nonNoteActions] = _.partition((action) => {
     return action.type === 'NOTE'
@@ -22,38 +24,7 @@ let arpeg = wrapScoringFunction((opts, score) => {
   return score
 })
 
-let config = wrapScoringFunction((opts, score) => {
-  return _.set('config', _.merge(score.config || {}, opts), score)
-})
-
-function flow(...args) {
-  if (_.isFunction(args[0])) {
-    return _.pipe(args)
-  }
-  let [thing, ...fns] = args
-  return _.pipe(fns)(getScore(thing))
-}
-
-let loop = wrapScoringFunction((score) => {
-  return _.set('loop', true, score)
-})
-
-function mix(...args) {
-  return mixScores(args)
-}
-
-let offset = wrapScoringFunction((amount, score) => {
-  score = _.cloneDeep(score)
-  score.actions.forEach(({ payload, type }) => {
-    if (type === 'NOOP') {
-      return
-    }
-    payload.offset = amount
-  })
-  return score
-})
-
-let arrange = wrapScoringFunction((handler, score) => {
+let arrange = wrapFn((handler, score) => {
   score = _.cloneDeep(score)
   score.actions.forEach(({ payload, type }) => {
     if (type === 'NOOP') {
@@ -65,16 +36,47 @@ let arrange = wrapScoringFunction((handler, score) => {
   return score
 })
 
-function seq(...args) {
+let config = wrapFn((opts, score) => {
+  return _.set('config', _.merge(score.config || {}, opts), score)
+})
+
+let flow = (...args) => {
+  if (_.isFunction(args[0])) {
+    return _.pipe(args)
+  }
+  let [thing, ...fns] = args
+  return _.pipe(fns)(getScore(thing))
+}
+
+let loop = wrapFn((score) => {
+  return _.set('loop', true, score)
+})
+
+let mix = (...args) => {
+  return mixScores(args)
+}
+
+let offset = wrapFn((amount, score) => {
+  score = _.cloneDeep(score)
+  score.actions.forEach(({ payload, type }) => {
+    if (type === 'NOOP') {
+      return
+    }
+    payload.offset = amount
+  })
+  return score
+})
+
+let seq = (...args) => {
   let [fns, scores] = _.partition(_.isFunction, args)
   return _.pipe(fns)(concatScores(scores))
 }
 
-let tempo = wrapScoringFunction((bpm, score) => {
+let tempo = wrapFn((bpm, score) => {
   return _.set('tempo', bpm, score)
 })
 
-let tran = wrapScoringFunction((amount, score) => {
+let tran = wrapFn((amount, score) => {
   score = _.cloneDeep(score)
   score.actions.forEach(({ payload }) => {
     if (payload.nn == null) {
@@ -86,14 +88,13 @@ let tran = wrapScoringFunction((amount, score) => {
 })
 
 module.exports = {
-  wrapScoringFunction,
   arpeg,
+  arrange,
   config,
   flow,
   loop,
   mix,
   offset,
-  arrange,
   seq,
   tempo,
   tran
