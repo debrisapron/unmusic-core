1ad8269866a308aafc95c20c3321a0f1c14cf08a
DEBRIS APRON <debrisapron@gmail.com>
2018-09-10 20:36:43 -0700

    checkpoint

--- src/getScore.js
+++
@@ -1,7 +0,0 @@
-import * as actionHelpers from './actionHelpers'
-
-export default function getScore(thing) {
-  return thing.actions
-    ? thing
-    : actionHelpers.wrap(actionHelpers.get(thing))
-}
